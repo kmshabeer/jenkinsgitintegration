2bec6a5fd15317f8d78616e660b64ddd5b6df216
kmshabeer <kmshabeerad@gmail.com>
2021-05-17 18:09:24 +0530

    third commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
+import java.io.*;
 
-public class Hello {
+class Hello {
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		System.out.println("Hello,World");
 
 	}
